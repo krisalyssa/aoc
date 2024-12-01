fd48423f581242ee76aa917400e746fd53a659fb
Kris Cottingham <krisalyssa@cottingham.net>
2024-11-30 21:07:58 -0600

    chore: add source files for Clojure

---
+++ lang/clojure/skeleton/src/day-23.clj
@@ -0,0 +1,12 @@
+(ns day-23
+  (:gen-class))
+
+(defn part1 [_filename]
+  0)
+
+(defn part2 [_filename]
+  0)
+
+(defn -main []
+  (printf "day 23 part 1: %d%n" (part1 "../data/23.txt"))
+  (printf "day 23 part 2: %d%n" (part2 "../data/23.txt")))
---
+++ lang/clojure/skeleton/src/day-25.clj
@@ -0,0 +1,12 @@
+(ns day-25
+  (:gen-class))
+
+(defn part1 [_filename]
+  0)
+
+(defn part2 [_filename]
+  0)
+
+(defn -main []
+  (printf "day 25 part 1: %d%n" (part1 "../data/25.txt"))
+  (printf "day 25 part 2: %d%n" (part2 "../data/25.txt")))
