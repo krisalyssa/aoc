98f5cc2380dc52f27e8cf9a09d98552a3e6409b6
Kris Cottingham <krisalyssa@cottingham.net>
2024-12-04 08:51:08 -0600

    chore: add alias to elixir test skeleton files

--- lang/elixir/skeleton/test/aoc/day_04_test.exs
+++ lang/elixir/skeleton/test/aoc/day_04_test.exs
@@ -3,18 +3,20 @@ defmodule AoC.Day04.Test do
 
   use ExUnit.Case, async: false
 
+  alias AoC.Day04
+
   # comment this out to always log to the console
   @moduletag :capture_log
 
   describe "part 1" do
     test "with sample data" do
-      assert AoC.Day04.part_1(["../data/04.txt"]) == 1
+      assert Day04.part_1(["../data/04.txt"]) == 1
     end
   end
 
   describe "part 2" do
     test "with sample data" do
-      assert AoC.Day04.part_2(["../data/04.txt"]) == 1
+      assert Day04.part_2(["../data/04.txt"]) == 1
     end
   end
 end
