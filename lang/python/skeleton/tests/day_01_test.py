84aeddff444614555309f6cdb1ea76d97f0ff030
Kris Cottingham <kris.cottingham@redfin.com>
2025-09-04 16:08:51 -0500

    feat: add initial skeleton files for python

---
+++ lang/python/skeleton/tests/day_01_test.py
@@ -0,0 +1,18 @@
+#!env python
+# code: language=python insertSpaces=true tabSize=2
+
+import unittest
+from aoc.day_01 import part1, part2
+
+class TestDay01(unittest.TestCase):
+
+    test_data = {}
+
+    def test_part1(self):
+        self.fail("implement test for part1")
+
+    def test_part2(self):
+        self.fail("implement test for part2")
+
+if __name__ == '__main__':
+    unittest.main()
