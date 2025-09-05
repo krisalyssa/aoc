10b1211b9641fce75a1d564527be05bc7ab206f6
Kris Cottingham <kris.cottingham@redfin.com>
2025-09-04 21:01:53 -0500

    chore: disable some Python linters

--- lang/python/skeleton/tests/day_01_test.py
+++ lang/python/skeleton/tests/day_01_test.py
@@ -2,11 +2,11 @@
 # code: language=python insertSpaces=true tabSize=2
 
 import unittest
-from aoc.day_01 import part1, part2
+
+# from aoc.day_01 import part1, part2
 
 
 class TestDay01(unittest.TestCase):
-
     test_data = {}
 
     def test_part1(self):
