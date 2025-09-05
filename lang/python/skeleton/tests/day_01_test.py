1976c3018bcc2ba06a74479e710041a6bd9c3bfe
Kris Cottingham <kris.cottingham@redfin.com>
2025-09-04 20:45:02 -0500

    fix: reformat Python files

--- lang/python/skeleton/tests/day_01_test.py
+++ lang/python/skeleton/tests/day_01_test.py
@@ -4,6 +4,7 @@
 import unittest
 from aoc.day_01 import part1, part2
 
+
 class TestDay01(unittest.TestCase):
 
     test_data = {}
@@ -14,5 +15,6 @@ class TestDay01(unittest.TestCase):
     def test_part2(self):
         self.fail("implement test for part2")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
