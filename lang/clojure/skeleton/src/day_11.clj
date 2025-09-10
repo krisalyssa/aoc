94f857c38469ce32ccda3e45228f076a71053148
Kris Cottingham <kris.cottingham@redfin.com>
2025-09-09 21:21:20 -0500

    chore: add skeleton files for clojure

---
+++ lang/clojure/skeleton/src/day_11.clj
@@ -0,0 +1,12 @@
+(ns day-11
+  (:gen-class))
+
+(defn part1 [_filename]
+  0)
+
+(defn part2 [_filename]
+  0)
+
+(defn -main []
+  (printf "day 11 part 1: %d%n" (part1 "../data/11.txt"))
+  (printf "day 11 part 2: %d%n" (part2 "../data/11.txt")))
