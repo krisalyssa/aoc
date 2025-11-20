bab7a47b1b3d5af700e07340d3880815c3822570
Kris Cottingham <kris.cottingham@redfin.com>
2025-11-19 23:19:45 -0600

    chore: improve day typescript files

--- lang/typescript/skeleton/days/07.ts
+++ lang/typescript/skeleton/days/07.ts
@@ -1,12 +1,13 @@
-export async function run() {
-  runPart1()
-  runPart2()
+export async function run(data: string) {
+  return [await runPart1(data), await runPart2(data)]
 }
 
-export async function runPart1() {
+export async function runPart1(_data: string) {
   console.log("inside day 07 part 1")
+  return undefined
 }
 
-export async function runPart2() {
+export async function runPart2(_data: string) {
   console.log("inside day 07 part 2")
+  return undefined
 }
